871aa160ac057ba58f4be72ac749b4f821c92613
Floranaras <carlosranara0@gmail.com>
2026-03-02 16:47:32 +0800

    refactor: doc and conv for ast

--- include/ast.h
+++ include/ast.h
@@ -8,102 +8,102 @@
 // =============================================================================
 
 typedef enum {
-    AST_PROGRAM,        // Root node
-    AST_NUMBER,         // Numeric literal
-    AST_STRING,         // String literal
-    AST_IDENTIFIER,     // Variable reference
-    AST_BINARY_OP,      // Binary operation (+, -, *, /, ==, etc.)
-    AST_UNARY_OP,       // Unary operation (-, +)
-    AST_ASSIGNMENT,     // Variable assignment
-    AST_IF_STMT,        // If statement
-    AST_WHILE_STMT,     // While loop
-    AST_FUNCTION_DEF,   // Function definition
-    AST_FUNCTION_CALL,  // Function call
-    AST_RETURN_STMT,    // Return statement
-    AST_PRINT_STMT,     // Print statement (built-in)
-    AST_BLOCK           // Block of statements
+	AST_PROGRAM,        // Root node
+	AST_NUMBER,         // Numeric literal
+	AST_STRING,         // String literal
+	AST_IDENTIFIER,     // Variable reference
+	AST_BINARY_OP,      // Binary operation (+, -, *, /, ==, etc.)
+	AST_UNARY_OP,       // Unary operation (-, +)
+	AST_ASSIGNMENT,     // Variable assignment
+	AST_IF_STMT,        // If statement
+	AST_WHILE_STMT,     // While loop
+	AST_FUNCTION_DEF,   // Function definition
+	AST_FUNCTION_CALL,  // Function call
+	AST_RETURN_STMT,    // Return statement
+	AST_PRINT_STMT,     // Print statement (built-in)
+	AST_BLOCK           // Block of statements
 } ASTNodeType;
 
 typedef struct ASTNode {
-    ASTNodeType type;
-    int line_number;
+	ASTNodeType type;
+	int line_number;
 
-    union {
-        // Literals
-        struct {
-            double value;
-        } number;
+	union {
+		// Literals
+		struct {
+			double value;
+		} number;
 
-        struct {
-            char* value;
-        } string;
+		struct {
+			char* value;
+		} string;
 
-        struct {
-            char* name;
-        } identifier;
+		struct {
+			char* name;
+		} identifier;
 
-        // Operations
-        struct {
-            struct ASTNode* left;
-            struct ASTNode* right;
-            TokenType operator;
-        } binary_op;
+		// Operations
+		struct {
+			struct ASTNode* left;
+			struct ASTNode* right;
+			TokenType operator;
+		} binary_op;
 
-        struct {
-            struct ASTNode* operand;
-            TokenType operator;
-        } unary_op;
+		struct {
+			struct ASTNode* operand;
+			TokenType operator;
+		} unary_op;
 
-        // Statements
-        struct {
-            char* variable;
-            struct ASTNode* value;
-        } assignment;
+		// Statements
+		struct {
+			char* variable;
+			struct ASTNode* value;
+		} assignment;
 
-        struct {
-            struct ASTNode* condition;
-            struct ASTNode* then_block;
-            struct ASTNode* else_block;  // Can be NULL
-        } if_stmt;
+		struct {
+			struct ASTNode* condition;
+			struct ASTNode* then_block;
+			struct ASTNode* else_block;  // Can be NULL
+		} if_stmt;
 
-        struct {
-            struct ASTNode* condition;
-            struct ASTNode* body;
-        } while_stmt;
+		struct {
+			struct ASTNode* condition;
+			struct ASTNode* body;
+		} while_stmt;
 
-        struct {
-            char* name;
-            char** parameters;      // Array of parameter names
-            int param_count;
-            struct ASTNode* body;
-        } function_def;
+		struct {
+			char* name;
+			char** parameters;      // Array of parameter names
+			int param_count;
+			struct ASTNode* body;
+		} function_def;
 
-        struct {
-            char* function_name;
-            struct ASTNode** arguments;
-            int arg_count;
-        } function_call;
+		struct {
+			char* function_name;
+			struct ASTNode** arguments;
+			int arg_count;
+		} function_call;
 
-        struct {
-            struct ASTNode* value;  // Can be NULL for bare return
-        } return_stmt;
+		struct {
+			struct ASTNode* value;  // Can be NULL for bare return
+		} return_stmt;
 
-        struct {
-            struct ASTNode* value;
-        } print_stmt;
+		struct {
+			struct ASTNode* value;
+		} print_stmt;
 
-        // Collections
-        struct {
-            struct ASTNode** statements;
-            int count;
-        } block;
+		// Collections
+		struct {
+			struct ASTNode** statements;
+			int count;
+		} block;
 
-        struct {
-            struct ASTNode** statements;
-            int count;
-        } program;
+		struct {
+			struct ASTNode** statements;
+			int count;
+		} program;
 
-    } data;
+	} data;
 } ASTNode;
 
 // Create a new AST node
